7cf74839f0348bf58a6ea962d25e1816ecfc8499
Jo Seongmin <ss662717@naver.com>
2022-05-24 19:24:57 +0900

    Update 12.cpp

---
+++ NewCppProject/NewCppProject/12.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "LinkedList.h"
+
+
+int main()
+{
+	// 연결형 리스트
+
+
+
+
+	return 0;
+}
+
+
+// 과제
+// 1. 가변배열
+// 스스로 구현 가능하게 복습할 것
+
+// 2. 가변배열 안에 넣은 데이터 정렬
+// - 버블 정렬
