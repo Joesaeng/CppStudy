f1ab15d3c96455e9feef11cb224a699a169d3a30
Jo Seongmin <ss662717@naver.com>
2022-05-24 15:27:00 +0900

    문자열

--- NewCppProject/NewCppProject/8.cpp
+++ NewCppProject/NewCppProject/8.cpp
@@ -2,6 +2,26 @@
 #include <stdio.h>
 #include <wchar.h>
 
+#include <assert.h>
+
+
+// ** 구현하고자 하는 기능을 고민해서 순서대로 정리해두는 습관을 가지자
+
+
+
+// 함수 오버로딩  : 함수의 이름은 같지만 인자의 타입이나 인자의 개수의 차이 등으로 동일한 이름의 여러가지의 함수가 만들어질 수 있는것
+void Test(int a)
+{
+
+}
+
+void Test(int a, float b)
+{
+
+}
+
+
+
 unsigned int GetLength(const wchar_t* _pStr)
 {
 	int i = 0; // 문자 개수 체크 변수
@@ -18,6 +38,27 @@ unsigned int GetLength(const wchar_t* _pStr)
 	return i;
 }
 
+void StrCat(wchar_t* _pDest, unsigned int _iBufferSize, const wchar_t* _pSrc)
+{
+	int iDestLen = GetLength(_pDest);
+	int iSrcLen = GetLength(_pSrc);
+	if (_iBufferSize < iDestLen + iSrcLen + 1)/*Null문자 공간까지 계산*/
+	{
+		assert(nullptr);
+	}
+	// 이어붙인 최종 문자열 길이가 목적지 저장 공간은 넘어서려는 경우 예외처리
+
+	// 문자열 이어 붙이기
+	// 1. Dest 문자열의 끝자리 찾기(문자열이 이어 붙을 자리) // Dest의 길이를 위에서 구해놓았기 때문에 Dest문자열의 끝자리는 그 길이를 인덱스로 가지는 자리가 된다.
+	// 2. 반복적으로 Src 문자열을 Dest 끝 위치에 복사하기
+	// 3. Src 문자열의 끝을 만나면 반복 종료
+	for (int i = 0; i < iSrcLen + 1; ++i)
+	{
+		_pDest[iDestLen + i] = _pSrc[i];
+	}
+
+}
+
 int main()
 {
 	wchar_t szName[10] = L"Seongmin";
@@ -26,7 +67,18 @@ int main()
 	//int iLen = wcslen(szName);
 	int iLen = GetLength(szName);
 
+
+	Test(1, 1.4f);
+	Test(1); // 함수 오버로딩
+
 	// 문자열 이어 붙이기
+	
+	wchar_t szString[10] = L"abc";
+	StrCat(szString, 10, L"def");
+	
+	int iRet = wcscmp(L"abc",L"abc");
+
+
 
 	return 0;
 }
---
+++ NewCppProject/NewCppProject/문자열과제풀이.cpp
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <wchar.h>
+
+// 양측의 문자열의 우열을 확인해서 우측이 높으면 1, 좌측이 높으면 -1, 동일하면 0을 가지는 함수
+// 1. 양측의 문자열을 받아온다.
+// 2. 양측의 각 인덱스별로 비교하는 반복문을 만든다 (우열은 아스키코드로 확인할수 있다. 아스키코드가 작은 경우 우선)
+// 3. 1번의 문자열을 비교 . 같은경우, 다른경우
+
+int wcCompare(const wchar_t* _left, const wchar_t* _right)
+{
+	int i = 0;
+	int j = 0;
+	while (_left[i] != 0 || _right[i] != 0)
+	{
+		if (_left[i] != _right[i])
+		{
+			_left[i] < _right[i] ? j = -1 : j = 1;
+			break;
+		}
+		else
+		{
+			++i;
+		}
+	}
+	return j;
+}
+
+
+int main()
+{
+	int i = wcCompare(L"abcde",L"abcdef");
+
+	return 0;
+}
