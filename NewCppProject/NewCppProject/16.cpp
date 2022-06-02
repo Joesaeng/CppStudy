025953f24f24ec2ad549d766dcf1e259a41beabf
Jo Seongmin <ss662717@naver.com>
2022-06-02 20:29:08 +0900

    이진탐색트리

--- NewCppProject/NewCppProject/16.cpp
+++ NewCppProject/NewCppProject/16.cpp
@@ -14,8 +14,22 @@ using std::endl;
 using std::wcout;
 using std::wstring;
 
-#define MAN		1
-#define WOMAN	2
+
+// 열거형
+enum class MY_TYPE
+{
+	TYPE_1, // 0
+	TYPE_2, // 1
+	TYPE_3, // 2
+	TYPE_4 = 100,
+	TYPE_5 // 101 *중간에 숫자를 지정하면 그 다음 타입에 숫자를지정하지않는다면 ++된다
+
+};
+
+enum class OTHER_TYPE
+{
+	TYPE_1,
+};
 
 // Tree : 트리
 // 자식 노드의 개수가 2개 이하인 트리를 이진트리라고 한다.
@@ -39,84 +53,19 @@ using std::wstring;
 // 1. 데이터가 정렬되있을때
 // 2. 범위를 절반씩 나눠가며 문제를 찾는 탐색
 
-struct tStdInfo
-{
-	wchar_t szName[20];
-	unsigned char age;
-	unsigned char gender;
-
-	tStdInfo()
-		: szName{}
-		, age(0)
-		, gender(0)
-	{}
-	tStdInfo(const wchar_t* _pName, unsigned char _age, unsigned char _gender)
-		: szName{}
-		, age(_age)
-		, gender(_gender)
-	{
-		wcscpy_s(szName, _pName);
-	}
-};
-
 
 int main()
 {
-	setlocale(LC_ALL, "korean");
-	_wsetlocale(LC_ALL, L"korean");
 
-	set<int> setInt;
-
-	setInt.insert(100);
-	setInt.insert(100);
-	setInt.insert(100);
-
-	map<const wchar_t*, tStdInfo> mapData;
-
-	tStdInfo info(L"김개똥", 20, MAN);
-	tStdInfo info2(L"홍길똥", 30, WOMAN);
-
-	mapData.insert(make_pair(L"김개똥", info));
-	mapData.insert(make_pair(L"홍길똥", info2));
-
-	map<const wchar_t*, tStdInfo>::iterator mapIter;
-	mapIter = mapData.find(L"임개똥");
-
-	if (mapIter == mapData.end())
-		wcout << L"데이터를 찾을수 없다." << endl;
-	else
-	{
-		wcout << L"이름 : " << mapIter->second.szName << endl;
-		wcout << L"나이 : " << mapIter->second.age << endl;
-		wcout << L"성별 : ";
-		mapIter->second.gender == MAN ? wcout << L"남자" << endl : wcout << L"여자" << endl;
-	}
-
-	map<wstring, tStdInfo> mapStdInfo;
-
-	wstring str;
-	str = L"abcdeFU ";
-	str += L"as your mom";
-	str[1] = L'v';
-
-	str = L"봄여름가을겨울";
-
-	wstring str2;
-	if (str < str2)
-	{
-
-	}
 
 	cBST<int, int> cBstInt;
-	tPair<int, int> pair;
 
-	pair.first = 100;
-	cBstInt.insert(pair);
-	pair.first = 150;
-	cBstInt.insert(pair);
-	pair.first = 50;
-	cBstInt.insert(pair);
+	cBstInt.insert(makeBstpair(100, 0));
+	cBstInt.insert(makeBstpair(150, 0));
+	cBstInt.insert(makeBstpair(50, 0));
 
+	cBST<int, int>::iterator Iter = cBstInt.begin();
+	Iter = cBstInt.find(150);
 
 	return 0;
 }
--- NewCppProject/NewCppProject/cBST.h
+++ NewCppProject/NewCppProject/cBST.h
@@ -1,11 +1,26 @@
 #pragma once
 
+
+enum class NODE_TYPE
+{
+	PARENT,
+	LCHILD,
+	RCHILD,
+	END,
+};
+
 template<typename T1, typename T2>
 struct tPair
 {
 	T1 first;
 	T2 second;
 };
+
+template<typename T1, typename T2>
+tPair<T1, T2> makeBstpair(const T1& _first, const T2& _second)
+{
+	return tPair<T1, T2>{_first, _second};
+}
 
 
 template<typename T1, typename T2>
@@ -14,10 +29,16 @@ struct tBSTNode
 	// data
 	tPair<T1,T2> pair;
 	// 부모노드
-	tBSTNode* pParent;
-	tBSTNode* pLeftChild;
-	tBSTNode* pRightChild;
+	tBSTNode*	arrNode[(int)NODE_TYPE::END];
 
+	tBSTNode()
+		:pair()
+		,arrNode()
+	{}
+	tBSTNode(const tPair<T1,T2>& _pair, tBSTNode* _pParent, tBSTNode* _pLchild, tBSTNode* _pRchild)
+		: pair(_pair)
+		, arrNode{ _pParent, _pLchild, _pRchild }
+	{}
 };
 
 template <typename T1, typename T2>
@@ -25,7 +46,7 @@ class cBST
 {
 private:
 	tBSTNode<T1,T2>*	pRoot; // 루트 노드 주소
-	int			iCount; // 데이터 개수
+	int					iCount; // 데이터 개수
 
 public:
 	bool insert(const tPair<T1, T2>& _pair);
@@ -34,17 +55,32 @@ public:
 		: pRoot(nullptr)
 		, iCount(0)
 	{}
+	class iterator;
+	iterator begin();
+	iterator end();
+	iterator find(const T1& _find);
+	// iterator
+	class iterator
+	{
+		cBST<T1, T2>*		pBST;
+		tBSTNode<T1, T2>*	iterNode; // null 인 경우 end iterator
 
+	public:
+		iterator()
+			: pBST(nullptr)
+			, iterNode(nullptr)
+		{}
+		iterator(cBST<T1,T2>* _pBST, tBSTNode<T1,T2>* _pNode)
+			: pBST(_pBST)
+			, iterNode(_pNode)
+		{}
+	 };
 };
 
 template<typename T1, typename T2>
 inline bool cBST<T1, T2>::insert(const tPair<T1, T2>& _pair)
 {
-	tBSTNode<T1,T2>* pNewNode = new tBSTNode<T1, T2>();
-	pNewNode->pair = _pair;
-	pNewNode->pParent = nullptr;
-	pNewNode->pLeftChild = nullptr;
-	pNewNode->pRightChild = nullptr;
+	tBSTNode<T1, T2>* pNewNode = new tBSTNode<T1, T2>(_pair, nullptr, nullptr, nullptr);
 
 	// 첫번째 데이터 라면
 	if (nullptr == pRoot)
@@ -54,36 +90,24 @@ inline bool cBST<T1, T2>::insert(const tPair<T1, T2>& _pair)
 	else
 	{
 		tBSTNode<T1, T2>* pNode = pRoot;
+		NODE_TYPE node_type = NODE_TYPE::END;
 		while (true)
 		{
 			if (pNode->pair.first < pNewNode->pair.first)
-			{
-				if (nullptr == pNode->pRightChild)
-				{
-					pNode->pRightChild = pNewNode;
-					pNewNode->pParent = pNode;
-					break;
-				}
-				else
-					pNode = pNode->pRightChild;
-
-
-			}
+				node_type = NODE_TYPE::RCHILD;
 			else if (pNode->pair.first > pNewNode->pair.first)
+				node_type = NODE_TYPE::LCHILD;
+			else
+				return false;
+
+			if (nullptr == pNode->arrNode[(int)node_type])
 			{
-				if (nullptr == pNode->pLeftChild)
-				{
-					pNode->pLeftChild = pNewNode;
-					pNewNode->pParent = pNode;
-					break;
-				}
-				else
-					pNode = pNode->pLeftChild;
+				pNode->arrNode[(int)node_type] = pNewNode;
+				pNewNode->arrNode[(int)NODE_TYPE::PARENT] = pNode;
+				break;
 			}
 			else
-			{
-				return false;
-			}
+				pNode = pNode->arrNode[(int)node_type];
 		}
 		
 	}
@@ -92,5 +116,51 @@ inline bool cBST<T1, T2>::insert(const tPair<T1, T2>& _pair)
 	++iCount;
 
 	return true;
+}
+
+template<typename T1, typename T2>
+inline typename cBST<T1,T2>::iterator cBST<T1, T2>::begin()
+{
+	tBSTNode<T1, T2>* pNode = pRoot;
+	while (pNode->arrNode[(int)NODE_TYPE::LCHILD])
+	{
+		pNode = pNode->arrNode[(int)NODE_TYPE::LCHILD];
+	}
+	
+	return iterator(this, pNode);
+}
+template<typename T1, typename T2>
+inline typename cBST<T1, T2>::iterator cBST<T1, T2>::end()
+{
+	return iterator(this, nullptr);
+}
+
+template<typename T1, typename T2>
+inline typename cBST<T1,T2>::iterator cBST<T1, T2>::find(const T1& _find)
+{
+	tBSTNode<T1, T2>* pNode = pRoot;
+	NODE_TYPE node_type = NODE_TYPE::END;
+
+	while (true)
+	{
+		if (pNode->pair.first < _find)
+			node_type = NODE_TYPE::RCHILD;
+		else if (pNode->pair.first > _find)
+			node_type = NODE_TYPE::LCHILD;
+		else
+		{
+			// 찾았다 : pNode가 find 노드이다
+			break;
+		}
+		if (nullptr == pNode->arrNode[(int)node_type])
+		{
+			// 찾지 못했다 : pNode == nullptr ==> enditerator
+			pNode = nullptr;
+			break;
+		}
+		else
+			pNode = pNode->arrNode[(int)node_type];
+	}
+	return iterator(this, pNode);
 }
 
